6d4de5942024d0cfcb4fb6409a2889110e6652e3
eakyel <e.akyel@outlook.com>
2022-04-30 19:40:59 +0300

    Create theme file

---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+export const typography = {
+  title: {
+    1: {
+      fontSize: "4.375rem",
+      fontWeight: "bold",
+    },
+    2: {
+      fontSize: "2.25rem",
+      fontWeight: "bold",
+    },
+  },
+};
