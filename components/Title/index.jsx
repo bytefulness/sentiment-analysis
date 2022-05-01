9baa238bceef3bb7f4fc327c56c28fcd333a4e4d
eakyel <e.akyel@outlook.com>
2022-05-01 16:00:25 +0300

    Update Title component

--- components/Title/index.jsx
+++ components/Title/index.jsx
@@ -1,29 +1,36 @@
 import PropTypes from "prop-types";
-import styledComponents from "styled-components";
-import { typography } from "styles/theme";
+import styled from "styled-components";
+import { typography } from "theme";
 
-const Title = ({ level, children }) => {
+const Title = ({ level, variant, children }) => {
   const Tag = `h${level}`;
   return (
-    <StyledTitle as={Tag} level={level}>
+    <StyledTitle as={Tag} level={level} variant={variant}>
       {children}
     </StyledTitle>
   );
 };
 
+const StyledTitle = styled(Title)`
+  font-size: ${(props) => typography.heading[props.variant].fontSize};
+  font-weight: ${(props) => typography.heading[props.variant].fontWeight};
+  line-height: ${(props) => typography.heading[props.variant].lineHeight};
+`;
+
 Title.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.string.isRequired,
+
+  /** Heading level */
   level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+
+  /** Variants from the design tokens */
+  variant: PropTypes.oneOf(["4xl", "3xl", "2xl", "xl", "lg", "md", "sm"]),
 };
 
 Title.defaultProps = {
   level: 1,
+  variant: "xl",
   children: null,
 };
 
 export default Title;
-
-const StyledTitle = styledComponents(Title)`
-    font-size: ${(props) => typography.title[props.level]?.fontSize};
-    font-weight: ${(props) => typography.title[props.level]?.fontWeight};
-`;
