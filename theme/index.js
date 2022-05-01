b54de63c1060a4613a5570c25f71c80169f1ccdb
eakyel <e.akyel@outlook.com>
2022-05-01 16:01:26 +0300

    Create design tokens

---
+++ theme/index.js
@@ -0,0 +1,126 @@
+export const lineHeight = {
+  base: 1,
+  sm: 1.2,
+  md: 1.5,
+};
+
+export const typography = {
+  // Heading Tokens
+  heading: {
+    "4xl": {
+      fontSize: "4.5rem",
+      lineHeight: lineHeight.base,
+    },
+    "3xl": {
+      fontSize: "3.75rem",
+      lineHeight: lineHeight.base,
+    },
+    "2xl": {
+      fontSize: "3rem",
+      lineHeight: lineHeight.base,
+    },
+    xl: {
+      fontSize: "2.25rem",
+      lineHeight: lineHeight.sm,
+    },
+    lg: {
+      fontSize: "1.5rem",
+      lineHeight: lineHeight.sm,
+    },
+    md: {
+      fontSize: "1.25rem",
+      lineHeight: lineHeight.sm,
+    },
+    sm: {
+      fontSize: "1rem",
+      lineHeight: lineHeight.sm,
+    },
+  },
+
+  // Body Tokens
+  body: {
+    "6xl": {
+      fontSize: "3.75rem",
+    },
+    "5xl": {
+      fontSize: "3rem",
+    },
+    "4xl": {
+      fontSize: "2.25rem",
+    },
+    "3xl": {
+      fontSize: "1.875rem",
+    },
+    "2xl": {
+      fontSize: "1.5rem",
+    },
+    xl: {
+      fontSize: "1.25rem",
+    },
+    lg: {
+      fontSize: "1.125rem",
+    },
+    md: {
+      fontSize: "1rem",
+    },
+    sm: {
+      fontSize: "0.875rem",
+    },
+    xs: {
+      fontSize: "0.75rem",
+    },
+  },
+};
+
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  blue: "#e6f5fa",
+  gray: "#f4f5f9",
+
+  grayPalette: {
+    50: "#f7fafc",
+    100: "#edf2f7",
+    200: "#e2e8f0",
+    300: "#cbd5e0",
+    400: "#a0aec0",
+    500: "#718096",
+    600: "#4a5568",
+    700: "#2d3748",
+    800: "#1a202c",
+    900: "#171923",
+  },
+
+  orangePalette: {
+    base: "#ffeee2",
+    50: "#fffaf0",
+  },
+
+  greenPalette: {
+    base: "#eefcef",
+    50: "#f0fff4",
+  },
+};
+
+export const radii = {
+  base: "0.5rem",
+  sm: "1rem",
+  md: "1.5rem",
+  lg: "2rem",
+  full: "9999px",
+};
+
+export const spaces = {
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  8: "2rem",
+  9: "2.25rem",
+  10: "2.5rem",
+  12: "3rem",
+  16: "4rem",
+  20: "5rem",
+};
