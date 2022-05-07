de11723b147d42e03e90a888a1be300daf57a28f
eakyel <e.akyel@outlook.com>
2022-05-07 20:49:03 +0300

    Update Layout component

--- layout/index.jsx
+++ layout/index.jsx
@@ -1,12 +1,27 @@
+import styled from "styled-components";
+import { menus } from "constant";
 import Menu from "./Menu";
+import { spaces } from "theme";
 
 const Layout = ({ children }) => {
   return (
-    <main>
-      <Menu />
-      <div>{children}</div>
-    </main>
+    <Main>
+      <Menu menus={menus} />
+      <Content>{children}</Content>
+    </Main>
   );
 };
+
+const Main = styled.main`
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  gap: ${spaces[8]};
+`;
+
+const Content = styled.div`
+  padding-top: ${spaces[10]};
+  width: 100%;
+  max-width: 1280px;
+`;
 
 export default Layout;
